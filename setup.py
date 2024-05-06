45c13126794ed101e362160fa680462351dc0481
AngeloFerrando <angelo.ferrando42@gmail.com>
2024-05-06 10:11:06 +0200

    version fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
   
 setup( 
     name='my_math_unimore', 
-    version='0.3', 
+    version='0.4', 
     description='A sample Python package to test CI/CD', 
     long_description='A sample Python package to test CI/CD',
     author='Angelo Ferrando', 
